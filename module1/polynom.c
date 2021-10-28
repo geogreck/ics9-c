768acfc72e493d785a588dae73758d033a51a61d
Georgy Grechko <georgre4ko@ya.ru>
2021-10-28 14:14:29 +0300

    task conditions

---
+++ module1/polynom.c
@@ -0,0 +1,48 @@
+
+#include <stdio.h>
+
+long my_pow(long int x, unsigned int n)
+{
+    long int a = x, p = 1;
+    while (n > 0)
+    {
+        if ((n & 1) != 0)
+            p *= a;
+        a *= a;
+        n >>= 1;
+    }
+    return p;
+}
+
+int main()
+{
+    long n, x0, x, a;
+    long der_sum = 0, sum = 0;
+    scanf("%ld%ld", &n, &x0);
+
+    if (x0 != 0){
+        x = my_pow(x0, n);
+
+        for(long i = n; i >= 0; i--){
+            scanf("%ld", &a);
+            sum += a * x;
+            x = x / x0;
+            der_sum += i * a * x;
+        }
+    }
+    else{
+        for(long i = n; i > 0; i--){
+            scanf("%ld", &a);
+        }
+        der_sum = a;
+        scanf("%ld", &a);
+        sum = a;
+        if (n == 0)
+            der_sum = 0;
+    }
+
+    printf("%ld\n", sum);
+    printf("%ld\n", der_sum);
+
+    return 0;
+}
