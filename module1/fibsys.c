1d839f8ceeed1745b0324d43bc244ee1b489ee84
Georgy Grechko <georgre4ko@ya.ru>
2021-10-28 14:54:22 +0300

    m1 task3 fix

--- module1/fibsys.c
+++ module1/fibsys.c
@@ -1,36 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 typedef long long ll;
 
-ll find_fib(ll x)
+ll find_fib(ll x, int* n)
 {
     ll a = 0, b = 1;
+    int k = 1;
     while ((a + b) <= x)
     {
         ll t = b;
         b += a;
         a = t;
+        k++;
     }
-
+    *n = k;
     return b;
+}
+
+void print_arr(int* m)
+{
+    int flag = 0;
+    for (int i = 100; i >= 0; i--)
+    {
+        if (m[i] || flag)
+        {
+            flag = 1;
+            printf("%d", m[i]);
+        }
+    }
 }
 
 int main()
 {
     ll x, f;
+    int n;
     scanf("%lld", &x);
+
+    int m[100] = {0};
+
 
    while (x)
     {
         if (x == 1)
-            printf("%lld\n", x--);
-        else{
-            f = find_fib(x);
+            m[0] = 1;
+        else
+        {
+            f = find_fib(x, &n);
+            m[n - 1] = 1;
             x -= f;
-            printf("10");
         }
     }
+
+    print_arr(m);
 
     return 0;
 }
