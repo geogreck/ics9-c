f48ea618684faf8914fbcc54f8baa6d16bcb5b35
Georgy Grechko <georgre4ko@ya.ru>
2021-10-28 14:34:04 +0300

    m1 task3

--- module1/fibsys.c
+++ module1/fibsys.c
@@ -1,78 +1,35 @@
-
 #include <stdio.h>
+#include <stdlib.h>
 
-void check(int* d1, int* d2, int* d3)
+typedef long long ll;
+
+ll find_fib(ll x)
 {
-    if (*d1 && *d2 && !*d3)
+    ll a = 0, b = 1;
+    while ((a + b) <= x)
     {
-        *d3 = 1;
-        *d2 = 0;
-        *d1 = 0;
+        ll t = b;
+        b += a;
+        a = t;
     }
-    //printf("%d %d %d\n", *d1, *d2, *d3);
-}
 
-void push(int* d1, int* d2, int* d3)
-{
-    printf("%d ", *d1);
-    *d1 = *d2;
-    *d2 = *d3;
-    scanf("%d", (&*d3));
+    return b;
 }
 
 int main()
 {
-    int n;
-    int d1 = 0, d2 = 0, d3 = 0;
-    scanf("%d", &n);
-    if (n == 1)
-    {
-        scanf("%d", &d1);
-    }
-    if (n == 2)
-    {
-        scanf("%d", &d1);
-        scanf("%d", &d2);
-    }
-    if (n >= 3)
-    {
-        scanf("%d", &d1);
-        scanf("%d", &d2);
-        scanf("%d", &d3);
-    }
-    //printf("%d %d %d\n", d1, d2, d3);
-    if (d1)
-    {
-        d1 = 0;
-        d2 = 1;
-    }
-    else
-    {
-        d1 = 1;
-    }
-    //printf("%d %d %d\n", d1, d2, d3);
-    check(&d1, &d2, &d3);
+    ll x, f;
+    scanf("%lld", &x);
 
-    for (int i = 0; i < n - 3; i++)
+   while (x)
     {
-        push(&d1, &d2, &d3);
-        check(&d1, &d2, &d3);
-    }
-    
-    printf("%d ", d1);
-
-    if (d2 && !d3)
-    {
-        printf("%d\n", d2);
-    }
-     else if (d2 || d3)
-    {
-        printf("%d ", d2);
-        printf("%d\n", d3);
-    }
-    else 
-    {
-        printf("\n");
+        if (x == 1)
+            printf("%lld\n", x--);
+        else{
+            f = find_fib(x);
+            x -= f;
+            printf("10");
+        }
     }
 
     return 0;
