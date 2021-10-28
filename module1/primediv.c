5db6b2e2bb4a3e00228de00a8549eba01c574e23
Georgy Grechko <georgre4ko@ya.ru>
2021-10-28 15:45:54 +0300

    m1 task 7

---
+++ module1/primediv.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <math.h>
+
+int primediv(int x)
+{
+    int* m = calloc((size_t)x , sizeof(int));
+    size_t i = 2;
+
+    while (i < sqrt(x))
+    {
+        if (!m[i])
+        {
+            for (size_t j = i * i; j <= (size_t)x; j += i)
+            {
+                m[j] = 1;
+            }
+        }
+        i++;
+    }
+
+    for (i = (size_t)x - 1; i > 0; i--)
+    {
+        if (!m[i])
+        {
+            return (int)i;
+        }
+    }
+
+    return 10;
+}
+
+int main()
+{
+    int x, div;
+    scanf("%d", &x);
+
+    div = primediv(x);
+
+    printf("%d\n", div);
+    return 0;
+}
