884022f398226bcc440f1decdba956c2e1f2cc61
Georgy Grechko <georgre4ko@ya.ru>
2021-10-28 19:25:38 +0300

    m1 task 10

---
+++ module1/maxarray.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare_ints(void* a, void* b)
+{
+    int arg1 = *(int*)a;
+    int arg2 = *(int*)b;
+ 
+    return (arg1 > arg2) - (arg1 < arg2); // possible shortcut
+}
+
+int compare_chars(void* a, void* b)
+{
+    int arg1 = *(char*)a;
+    int arg2 = *(char*)b;
+ 
+    return (arg1 > arg2) - (arg1 < arg2); // possible shortcut
+}
+
+int maxarray(void *base, size_t nel, size_t width, int (*compare)(void *a, void *b))
+{
+    unsigned char* max = malloc(width);
+    size_t i_max = 0;
+
+    *max = *(unsigned char*)base;
+    for (size_t i = 1; i < nel; i++)
+    {
+        if (compare(((unsigned char*)base + i * width), max) > 0)
+        {
+            *max = *((unsigned char*)base + i * width);
+            i_max = i;
+        }
+    }
+
+    return (int)i_max;
+}
+
+int main()
+{
+    int arr[10] = {1, 2, 3, 4, 5, 110, 7, 8, 9, 10};
+    char arr_s[4] = {'a', 'b', 'v', 'd'};
+
+    printf("%d\n", maxarray(arr, 10, sizeof(int), compare_ints));
+    printf("%d\n", maxarray(arr_s, 4, sizeof(char), compare_chars));
+
+    return 0;
+}
